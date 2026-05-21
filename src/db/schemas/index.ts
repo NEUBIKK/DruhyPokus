bb2d8fd5512e9dedbed9b6d17c9dedb00e3c3409
NEUBIKK <adam.neubik1@gmail.com>
2026-05-21 16:02:29 +0200

    Added inzerat db table

--- src/db/schemas/index.ts
+++ src/db/schemas/index.ts
@@ -1,1 +1,3 @@
+// db/schemas/index.ts
+export * from "./inzrat.schema";
 export * from "./system-setting.schema";
---
+++ src/db/schemas/inzrat.schema.ts
@@ -0,0 +1,16 @@
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const items = sqliteTable("items", {
+  id: int().primaryKey({ autoIncrement: true }),
+  title: text().notNull(),
+  description: text(),
+  category: text().notNull(),
+  price: int(),
+  status: text(),
+  contactName: text().notNull(),
+  image: text(),
+  email: text(),
+});
+
+export type Item = typeof items.$inferSelect;
+export type NewItem = typeof items.$inferInsert;
