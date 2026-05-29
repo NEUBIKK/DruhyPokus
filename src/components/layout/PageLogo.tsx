37230943058776513415d3b1303bbe54a81b91bd
NEUBIKK <adam.neubik1@gmail.com>
2026-05-29 10:30:15 +0200

    Added Blogic logo for DARK MODE

--- src/components/layout/PageLogo.tsx
+++ src/components/layout/PageLogo.tsx
@@ -2,9 +2,18 @@
 
 import Image from "next/image";
 import { useTranslations } from "next-intl";
+import { useComputedColorScheme } from "@mantine/core";
 
 export function PageLogo() {
   const t = useTranslations();
+  const computedColorScheme = useComputedColorScheme("light");
 
-  return <Image src="/blogic-logo.png" alt={t("common.pageLogo.ariaLabel")} width={115} height={46} />;
+  return (
+    <Image
+      src={computedColorScheme === "dark" ? "/ChatGPT_Image_29._5._2026_10_23_26-removebg-preview.png" : "/blogic-logo.png"}
+      alt={t("common.pageLogo.ariaLabel")}
+      width={115}
+      height={46}
+    />
+  );
 }
